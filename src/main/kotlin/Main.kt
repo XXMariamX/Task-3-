936385ae9463e6bd8555fe5aa20586c37508875f
M <rura.essam@hotmail.com>
2022-08-16 10:35:03 +0200

    Task 3 OOP

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+abstract class Car(val model:String, val color: String,val speed:Int):Pricable {
+    abstract fun getFuelType()
+}
+class BMW( model:String,  color: String, speed:Int):Car(model,color,speed) {
+    override fun getFuelType() {
+        println("Fuel is gasoline.")
+    }
+
+    override fun printPrice() {
+        println("It cost $100000")
+    }
+}
+class Tesla( model:String,  color: String, speed:Int):Car(model,color,speed) {
+    override fun getFuelType() {
+        println("Fuel is electricity.")
+    }
+    override fun printPrice() {
+        println("It cost $10000000000")
+    }
+}
+interface Pricable {
+    fun printPrice()
+}
+
+fun main(args: Array<String>) {
+    var car1:Car=BMW("2020","White",220)
+    var car2:Car=Tesla("2022","Red",300)
+    car1.getFuelType()
+    car2.getFuelType()
+
+    car1.printPrice()
+    car2.printPrice()
+
+
+
+
+
+
+
+
+}
